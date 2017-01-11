5d46ea9fb18b250c0cdb1c89c005c0d8bb54ca11
bernard cooley <bernard cooley>
2017-01-11 23:21:28 +0000

    videos and photos carousels

--- cooleymusic/public/controllers/musicController.js
+++ cooleymusic/public/controllers/musicController.js
@@ -1,13 +1,3 @@
 myApp.controller('MusicController', function($scope, $http, $location) {
 	console.log("Music controller");
-
-	$scope.urlList = [
-	'https://www.youtube.com/watch?v=6EjrdBd08VU',
-	'https://www.youtube.com/watch?v=PrnI1QCj7jM',
-	'https://www.youtube.com/watch?v=hja7OLQy3IQ',
-	'https://www.youtube.com/watch?v=uhuF4Wp86lY'
-	];
-
-
-
 });
--- cooleymusic/public/controllers/videosController.js
+++ cooleymusic/public/controllers/videosController.js
@@ -1,3 +1,30 @@
 myApp.controller('VideosController', function($scope, $http, $location) {
 	console.log("Videos controller");
+
+	$scope.urlList = [
+	//Ableton jam
+	"https://www.youtube.com/embed/hja7OLQy3IQ",
+
+	//Slipstream
+	'https://www.youtube.com/embed/EivBI--j8Io',
+	'https://www.youtube.com/embed/uhuF4Wp86lY',
+	'https://www.youtube.com/embed/KsWDhgrBpIc',
+
+	//Imagination Land
+	'https://www.youtube.com/embed/6EjrdBd08VU',
+
+	//Urban Deception
+	'https://www.youtube.com/embed/PrnI1QCj7jM',
+
+	//Tales of Lazarus
+	'https://www.youtube.com/embed/_WyFbLsOoLo',
+	
+	//Escape from decadence
+	'https://www.youtube.com/embed/FghcZLpnIb0',
+	
+	//30 min mix
+	'https://www.youtube.com/embed/XEhRTNEfbu0',
+	'https://www.youtube.com/embed/RAUqNkileO8',
+	'https://www.youtube.com/embed/oC_AAHcxp2I',
+	];
 });
