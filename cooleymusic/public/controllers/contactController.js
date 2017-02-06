4c85222c67bca6369b8c27cd0fccb5a2d2c3fcc4
Bernard Cooley <bernardcooley@gmail.com>
2017-02-06 00:44:29 +0000

    sending prompt added

--- cooleymusic/public/controllers/contactController.js
+++ cooleymusic/public/controllers/contactController.js
@@ -1,8 +1,16 @@
 myApp.controller('ContactController', function($scope, $http, $location) {
 	console.log("Contact controller");
 		$scope.sendMessage = function() {
+		$scope.successMessage = "Sending...";
 		console.log("send message called: " + $scope.contact.email);
 		$http.post('http://localhost:3000/send', $scope.contact).success(function(data){
+			// console.log("Contact response: " + data);
+				if(data == "sent") {
+					$scope.successMessage = "Thank you for your interest";
+					console.log("Contact form submitted");
+					$scope.contact = null;
+					$scope.check = false;
+				}
 			});
 	};
 
@@ -13,11 +21,11 @@ myApp.controller('ContactController', function($scope, $http, $location) {
 			var emailAddress = $scope.contact.email;
 			console.log("Validate existing email called " + emailAddress);
 			$http.get('/mailing_list/' + emailAddress).success(function(response) {
-				console.log("Response: " + response);
+				console.log("Email check response: " + response);
 				if(response == null || emailAddress == '') {
 					console.log("Email: " + emailAddress);
 					$http.post('/mailing_list', {email: emailAddress}).success(function(response) {
-						console.log(response);
+						console.log("Contact response: " + response);
 					});	
 				}
 			});
