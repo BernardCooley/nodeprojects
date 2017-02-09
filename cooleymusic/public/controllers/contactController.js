bb2f2bce78dcdce507e9f09276fbf90ab682d60e
Bernard Cooley <bernardcooley@gmail.com>
2017-02-09 00:34:50 +0000

    contact fixed changed success message

--- cooleymusic/public/controllers/contactController.js
+++ cooleymusic/public/controllers/contactController.js
@@ -1,17 +1,18 @@
-myApp.controller('ContactController', function($scope, $http, $location) {
+myApp.controller('ContactController', function($scope, $http, $location, $timeout) {
+	$scope.successMessage = "Send message";
 	console.log("Contact controller");
 		$scope.sendMessage = function() {
-		// $scope.successMessage = "Sending...";
-		// console.log("send message called: " + $scope.contact.email);
-		// $http.post('http://localhost:3000/send', $scope.contact).success(function(data){
-		// 	// console.log("Contact response: " + data);
-		// 	if(data == "sent") {
-		// 		$scope.successMessage = "Thank you for your interest";
-		// 		console.log("Contact form submitted");
-		// 		$scope.contact = null;
-		// 		$scope.check = false;
-		// 	}
-		// });
+		$scope.successMessage = "Sending...";
+		console.log("send message called: " + $scope.contact.email);
+		$http.post('http://localhost:3000/send', $scope.contact).success(function(data){
+			if(data == "sent") {
+				$scope.successMessage = "Message sent";
+				$timeout(function () { $scope.successMessage = "Send message"; }, 3000);
+				console.log("Contact form submitted");
+				$scope.contact = null;
+				$scope.check = false;
+			}
+		});
 		$http.post('/storeMessage', $scope.contact).success(function(data){
 			
 		});
