5a4d38297d1b591ad6f958e9ca30de6c3158ec0c
Bernard Cooley <bernardcooley@Bernards-MacBook-Pro.local>
2017-02-24 00:09:23 +0000

    releases started

--- cooleymusic/public/controllers/releasesController.js
+++ cooleymusic/public/controllers/releasesController.js
@@ -1,3 +1,26 @@
 myApp.controller('ReleasesController', function($scope, $http, $location) {
 	console.log("Releases controller");
+
+	$scope.releases = [
+	  {
+	    "title":"Slipstream",
+	    "releaseDate":"2016-10-24",
+	    "label":"Bacci Bros Records",
+	    "mixes":["Original","thatboytim", "Luca Elle"],
+	    "links":[{"Beatport":"https://www.beatport.com/release/slipstream-ep/1865301", 
+	    	"Traxsource":"https://www.traxsource.com/title/695693/slipstream-ep",
+	    	"Juno Download":"http://www.junodownload.com/products/cooley-slipstream-ep/3232771-02/",
+	    	"Amazon":"https://www.amazon.com/Slipstream-EP-Cooley/dp/B01M5BA8AS"}
+	    	]
+
+	  },
+	  {
+	    "title":"Self Portrait",
+	    "releaseDate":"2017-03-06",
+	    "label":"Bacci Bros Records",
+	    "mixes":["Original","Luca Elle"]
+	  }
+	]
+
+
 });
